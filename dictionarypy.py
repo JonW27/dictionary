559fcafb205936551162378da7eeaa7fdcfe3534
Jonathan Wong <18wongj@gmail.com>
2015-10-18 18:27:40 -0400

    Update dictionarypy.py

--- dictionarypy.py
+++ dictionarypy.py
@@ -1,12 +1,21 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 
-word = input("Choose a word for a simple define\n Please put double quotation marks around the word. \n")
-url = 'http://www.thesaurus.com/browse/'+word+'?s=t'
-response = requests.get(url)
-html = response.content
+word = input("Choose words for a simple define. Please put double quotations around your words and leave a space between each. The definitions will be in opposite order when they appear\n")
+word = word.split()
+count = len(word)
+count = count - 1
+signal = -1
+while (signal < count):
+	url = 'http://www.thesaurus.com/browse/'+word[count]+'?s=t'
+	response = requests.get(url)
+	html = response.content
+	soup = BeautifulSoup(html)
+	table = soup.find('strong', attrs={'class': 'ttl'})
+	print table.text.replace('&nbsp;','')
+	count = count - 1
 
-soup = BeautifulSoup(html)
-table = soup.find('strong', attrs={'class': 'ttl'})
 
-print table.text.replace('&nbsp;','')
+
+
+
